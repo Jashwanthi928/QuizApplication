a85424268ae2bb4eb0662ad0d85701b59304da53
DebarunMitra <debarun96mitra@gmail.com>
2019-09-13 16:18:51 +0530

    Version 1.0
    randomly question select

--- js/script.js
+++ js/script.js
@@ -16,6 +16,24 @@ function barAccess()
   }
 }
 $(document).ready(function(){
+  let qcount=0,op=4;
   // $('#myModal').modal('show');
+  // $.getJSON("quizQuestions.json", function(result){
+  //      $.each(result, function(i, field){
+  //           //create table
+  //       });
+  //   });
+    let jsonValues = $.getJSON("quizQuestions.json", function(){
+    $("#optionBtn").empty();
+    let dataToStore = JSON.stringify(jsonValues.responseJSON.Personalities);
+    let totalQue=JSON.parse(dataToStore);
+    let queNo=Math.floor(Math.random() * totalQue.length);
+    $('.questions').html(totalQue[queNo].question);
+    for (i = 1; i <=op; i++) {
+    let btn='<div class="">'+'<input type="button" class="option" name="Option" value="'+totalQue[queNo][i]+'"/>'+'</div>';
+    $('#optionBtn').append(btn).last();
+     //console.log(totalQue[queNo][i]);
+    };
+    });
 
 });
