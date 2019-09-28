3f3fe0b6ee1f64d3f6945adc862410f36026bad6
DebarunMitra <debarun96mitra@gmail.com>
2019-09-28 23:53:32 +0530

    Version 1.1
    script.js file updated , api call through ajax

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 //navbar icon start
 let bar = document.getElementById('myLinks');
 const gradeMsg = ["Improve!!", "GOOD", "GREAT!!"]
-let userAns = new Array();
+let userAns = new Array();let st;
 
 function barAccess() {
   if (bar.style.display == "block") {
@@ -16,7 +16,6 @@ function barAccess() {
 //navbar icon end
 //countDown start
 document.getElementById('timeCounter').innerHTML = 9 + ":" + 59;
-
 function startTimer() {
   let presentTime = document.getElementById('timeCounter').innerHTML;
   let timeArray = presentTime.split(/[:]+/);
@@ -26,9 +25,8 @@ function startTimer() {
     m = m - 1
   }
   document.getElementById('timeCounter').innerHTML = m + ":" + s;
-  setTimeout(startTimer, 1500);
+  st=setTimeout(startTimer, 1000);
 }
-
 function checkSecond(sec) {
   if (sec < 10 && sec >= 0) {
     sec = "0" + sec
@@ -39,10 +37,9 @@ function checkSecond(sec) {
   return sec;
 }
 //countDown stop
-
-function clickedOption(id, que) {
+function clickedOption(id, que, qset,qid) {
   let optionVal = document.getElementById(id);
-  userAns[que - 1] = optionVal.value;
+  userAns[que] ={"q_set":qset,"qid":qid,"ans":optionVal.value};
   for (i = 1; i <= 4; i++) {
     if (i != id) {
       document.getElementById(i).style.border = "solid";
@@ -51,69 +48,51 @@ function clickedOption(id, que) {
       optionVal.style.border = "solid";
       optionVal.style.outline = "none";
       optionVal.style.borderColor = "#374176";
-      localStorage.setItem("ans", JSON.stringify(userAns));
+      localStorage.setItem("ans",JSON.stringify(userAns));
     }
   }
 }
 $(document).ready(function() {
-  let qcount = 10,
-    op = 4,
-    count = 0,
-    nextPrev = 1;
+  let qcount = 10,op = 4,count = 0,nextPrev = 1;
+  let opArr=['a','b','c','d'];
   let checkRepeat = new Array();
   checkRepeat = [];
   $('.total-que').html(qcount);
   $("#optionBtn").empty();
   $('.questions').html(' ');
   $('#startModal').modal('show');
-
+  /*question load  start*/
   $('.start-btn').click(function() {
-    $('#startModal').modal('hide');
-    $('#instruction').css("display", "block");
-    startTimer();
-    randomQuestionLoad();
-//    getQuestions(1);
-  });
-/*random question set start*/
-let randomQuestionLoad= function(){
-  let jsonValues = $.getJSON("quizQuestions.json", function() {
-    for (i = 1; i <= qcount; i++) {
-      let queNo = Math.floor(Math.random() * jsonValues.responseJSON.Personalities.length);
-        if (checkRepeat.includes(queNo)) {
-        i = i - 2;
-        if (count >= qcount) {
-          break;
-        }
-      } else {
-        if (count >= qcount) {
-          break;
-        }
-        count = count + 1;
-        checkRepeat.push(queNo);
-        let dataToStore = JSON.stringify(jsonValues.responseJSON.Personalities[queNo]);
-        localStorage.setItem(count, dataToStore);
-      }
-    }
-  });
-}
-/*random question set end*/
+      $.ajax({
+           url:'http://localhost:5020/ranQue/'
+         }).done(function(data) {
+             $('#startModal').modal('hide');
+             $('#instruction').css("display", "block");
+             localStorage.setItem('qSet',data);
+             startTimer();
+         });
+         getQuestions(1);
+    });
+    /*question load  end*/
+
 /*question-option show start*/
 let getQuestions = function(qc) {
     $("#optionBtn").empty();
     $('.questions').html(' ');
-    let retrievedObject = localStorage.getItem(qc);
+    let retrievedObject = localStorage.getItem("qSet");
     let parsedObject = JSON.parse(retrievedObject);
-    $('.questions').html(parsedObject.question);
-    //console.log(parsedObject);
+    $('.questions').html(parsedObject[qc-1].q);
     $('.question-number').html('Question ' + qc + '/');
     for (j = 1; j <= op; j++) {
-    let btn = '<div class="">' + '<input id="' + j + '" type="button" class="option" onclick="clickedOption(' + j + ',' + qc + ')" name="Option" value="' + parsedObject[j] + '"/>' + '</div>';
+    let btn = '<div class="">' + '<input id="' + j +'" type="button" class="option" onclick="clickedOption(' + j + ',' + (qc-1) +','+'\''+parsedObject[qc-1].qset+'\''+','+parsedObject[qc-1].qid +')" name="Option" value="' + parsedObject[qc-1][opArr[j-1]] + '"/>' + '</div>';
     $('#optionBtn').append(btn).last();
   }
 }
-  //getQuestions(1);
-  /*question-option show end*/
-  /*next question start*/
+/*question-option show end*/
+/*show score start*/
+//let
+/*show score end*/
+/*next question start*/
   $('.next').click(function() {
     nextPrev += 1;
     if (nextPrev <= qcount) {
@@ -128,7 +107,14 @@ let getQuestions = function(qc) {
       alert('You have seen all questions');
     }
   });
-  /*next question end*/
+/*next question end*/
+/*instruction to next start*/
+  $('.next-go').click(function() {
+  $('#instruction').css("display", "none");
+  $('#questDiv').css("display", "block");
+  getQuestions(1);
+  });
+/*instruction to next end*/
   /*prev question start*/
   $('.prev').click(function() {
     nextPrev -= 1;
@@ -143,47 +129,46 @@ let getQuestions = function(qc) {
       alert('This is the first question');
     }
   });
-  /*next question end*/
-
+/*next question end*/
   $("#submitAns").click(function() {
-    $("#questDiv").css("display", "none");
-    $("#userScore").css("display", "block");
-    $(".timeCounter").css("display", "none");
-    $(".timer-count").html('00:00');
-    //  clearInterval(startTimer);
-    let point = 0;
-    for (i = 1; i <= qcount; i++) {
-      let retrievedObject = localStorage.getItem(i);
-      let parsedObject = JSON.parse(retrievedObject);
-      if (userAns.includes(parsedObject.ans)) {
-        point += 1;
-      }
-    }
-    if (point < 5) {
-      $('.grade').html(gradeMsg[0]);
-    } else if (point == 5) {
-      $('.grade').html(gradeMsg[1]);
-    } else if (point > 5) {
-      $('.grade').html(gradeMsg[2]);
-    }
-    $('.score-point').html(`${point}/${qcount}`);
+  $("#userScore").css("display", "block");
+  $("#questDiv,.timeCounter").css("display", "none");
+  $(".timer-count").html('00:00');
+  clearInterval(st);
+  let point = 0;
+  let ans=localStorage.getItem('ans');
+  $.ajax({
+    type:'POST',
+    dataType: 'json',
+    contentType: "application/json",
+    data:ans,
+    url:'http://localhost:5020/checkAns'
+  }).done((data)=>{
+   point=parseInt(data);
+     if (point < 5) {
+       $('.grade').html(gradeMsg[0]);
+     } else if (point == 5) {
+       $('.grade').html(gradeMsg[1]);
+     } else if (point > 5) {
+       $('.grade').html(gradeMsg[2]);
+     }
+     $('.score-point').html(`${point}/${qcount}`);
+    }).fail((xhr, textStatus, errorThrown)=>{
+    console.log("ERROR: ",xhr.responseText)
+    return xhr.responseText;
+});
   });
   /*quit start*/
   $('.quit-btn').click(function() {
     $('#myModal').modal('show');
   });
   $('#ok').click(function() {
-    //  localStorage.clear();
+    localStorage.clear();
     $('#myModal').modal('hide');
     location.reload();
   });
   /*quit stop*/
   $('.review-ans').click(function() {
     location.reload();
-  });
-  $('.next-go').click(function() {
-    $('#instruction').css("display", "none");
-    $('#questDiv').css("display", "block");
-     getQuestions(1);
   });
 });
