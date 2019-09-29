61fa492d49fbe384e79780c12d6dd1a542774f6c
DebarunMitra <debarun96mitra@gmail.com>
2019-09-29 23:10:24 +0530

    Version 1.1
    backend main page

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
-let bar=document.getElementById('myLinks');
-function barAccess()
-{
- console.log('in');
- if(bar.style.display=="block")
- {
-   console.log("none");
-   bar.style.display="none";
- }
- else {
-       console.log("block");
-   bar.style.display="block";
- }
-}
-$(document).ready(function(){
- $('#myModal').modal('show');
-/*  $(window).load(function(){
-                $('#exampleModal').modal('show');
-            });*/
-       //     $('#myModal').on('click', function () {
-         //     $('#myInput').trigger('focus')
-           // })
-     /*$(window).load(function(){
-        $('#myModal').modal('show');
-      });*/
+const express=require('express');
+const mongodb=require('mongodb').MongoClient;
+const bodyParser=require('body-parser');
+const JSON = require('circular-json');
+const cors=require('cors');
+
+const app=express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
+app.use(cors());
+app.use(bodyParser.json());//initializing bodyParser middleware
 
+const db=require('./config/db');
 
- function start(startbutton){
-     	x= document.getElementById("startpage")
-	x.style.display="none";
-    
- 
- if(startbutton==='c'){
-     	y= document.getElementById("homepage")
-	y.style.display="block";
-    z= document.getElementById("instructionpage")
-	z.style.display="block";
-    
- }}
+const path=require('path');//to access public directory
+
+const port=5020;
+
+mongodb.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    if (err)
+        return console.log(err);
+    const database =db.db("quizOn").collection("questions");
+    require('./routesJS/routes')(app, database);
+    app.listen(port, () => {
+        console.log('Database Connected Successfully');
+    });
+});
