b7a0b1a2adc0e384558926343597154139641a8b
DebarunMitra <debarun96mitra@gmail.com>
2019-10-15 22:22:06 +0530

    Version 1.2

--- config/db.js
+++
@@ -1,6 +0,0 @@
-// Author: Debarun Mitra
-// Technology used: HTML, CSS, JavaScript, JQuery, Bootstrap
-// objective: Create a quiz application
-module.exports={
-  url:"mongodb+srv://quizon:quiz2on@cluster-0-zpc9p.mongodb.net/test?retryWrites=true&w=majority"
-}
